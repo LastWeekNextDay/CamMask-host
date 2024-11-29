c30421da1b688809d868b7204a8e967797de508d
LastWeekNextDay <doesn@mail.ru>
2024-11-29 17:25:33 +0200

    Second potential Firebase rollout failure fix
    Continuing fix from 7dc340b

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,14 @@
 const functions = require('firebase-functions');
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 8080;
 
 exports.test = functions.https.onRequest((req, res) => {
     res.send('Hello from Firebase!');
 });
+
+app.listen(port, () => {
+    console.log(`Running on port ${port}.`);
+});
+
